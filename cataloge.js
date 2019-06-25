7792fc2d67ae1658774979ea56cae12b818123f9
GrandEnfant <37689523+GrandEnfant@users.noreply.github.com>
2019-06-25 18:43:38 +0300

    в.0.0.2

--- cataloge.js
+++ cataloge.js
@@ -1,12 +1,12 @@
-Vue.component('product_box', {
+Vue.component('products', {
     data() {
         return {
             linkCatalog: "/catalogProducts.json",
             products: [],
         }
     },
-    mounted() {
-        this.getJson(`${API + this.linkCatalog}`)
+    mounted(){
+        this.$parent.getJson(`${API + this.linkCatalog}`)
             .then(data => {
             for(let el of data){
                 this.products.push(el);
@@ -20,14 +20,14 @@ Vue.component('product_box', {
         })
     },
     template:     
-    `<div class="product" v-for="item of products">
+    `<div><div class="product" v-for="item of products">
             <a href="singlePage.html" class="product__link"><img class="product__img" src="img/Layer2.jpg" alt="product"></a>
             <div class="product__info">
                 <a href="singlePage.html">
                     <p class="product__text">{{item.product_name}}</p>
                 </a><span class="product__price">{{item.price}} </span></div>
             <a href="#" class="product__add"><img :src=item.src_img alt="cart add">add to cart</a>
-        </div>`
+        </div> </div>`
 })
 
 
--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const API = `https://raw.githubusercontent.com/GrandEnfant/I-shop/master/db`;
 
 const app = new Vue({
- el: ".app",
+ el: "#app",
  data: {
-     products: [],
+//     products: [],
  },
     methods: {
         getJson(url) {
             return fetch(url)
                 .then(result => result.json())
+            
                 .catch(error => console.log(error))
             },
  
