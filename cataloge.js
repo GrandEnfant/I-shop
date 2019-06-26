4e06d9cf62e7d7a7af7fbf21afe5731ea4762a50
GrandEnfant <37689523+GrandEnfant@users.noreply.github.com>
2019-06-26 14:27:19 +0300

    v0.0.5

--- cataloge.js
+++ cataloge.js
@@ -1,27 +1,46 @@
 Vue.component('products', {
-    data() {
-        return {
-            linkCatalog: "/catalogProducts.json",
-            products: [],
+    data(){
+      return {
+          catalogUrl: `/catalogProducts.json`,
+          products: [],
+          filtered: [],
+          imgCatalog: `https://placehold.it/200x150`,
+      }
+    },
+    methods: {
+        filter(){
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
     mounted(){
-        this.$parent.getJson(`${API + this.linkCatalog}`)
+        this.$parent.getJson(`${API + this.catalogUrl}`)
             .then(data => {
-            for(let el of data){
-                this.products.push(el);
-            }
-        });
-        this.$parent.getJson(`catalogProducts.json`) 
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+        this.$parent.getJson(`getProducts.json`)
             .then(data => {
-            for(let el of data) {
-                this.products.push(el);
-            }
-        })
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            })
     },
-    template:     
-    `<div><div class="product" v-for="item of products">
-            <a href="singlePage.html" class="product__link"><img class="product__img" src="img/Layer2.jpg" alt="product"></a>
+    template: `<div class="products">
+        <product 
+        v-for="product of filtered" 
+        :key="product.id_product"
+        :product="product"
+        :img="imgCatalog"></product>
+    </div>`
+});
+Vue.component('product', {
+    props: ['product', 'img'],
+    template: `<div><div class="product" v-for="item of products">
+            <a href="singlePage.html" class="product__link"><img class="product__img" src="item.src_img" alt="product"></a>
             <div class="product__info">
                 <a href="singlePage.html">
                     <p class="product__text">{{item.product_name}}</p>
@@ -30,6 +49,32 @@ Vue.component('products', {
         </div> </div>`
 })
 
+
+//Vue.component('products', {
+//    data() {
+//        return {
+//            linkCatalog: "/catalogProducts.json",
+//            products: [],
+//        }
+//    },
+//    mounted(){
+//        this.$parent.getJson(`${API + this.linkCatalog}`)
+//            .then(data => {
+//            for(let el of data){
+//                this.products.push(el);
+//            }
+//        });
+//        this.$parent.getJson(`catalogProducts.json`) 
+//            .then(data => {
+//            for(let el of data) {
+//                this.products.push(el);
+//            }
+//        })
+//    },
+//    template:     
+// 
+//})
+//
 
 
 
--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const API = `https://raw.githubusercontent.com/GrandEnfant/I-shop/master/db`;
 
 const app = new Vue({
- el: "#app",
- data: {
-//     products: [],
- },
+    el: '#app',
+    data: {
+        userSearch: '',
+    },
     methods: {
-        getJson(url) {
+        getJson(url){
             return fetch(url)
                 .then(result => result.json())
-            
                 .catch(error => console.log(error))
-            },
- 
-}
+        },
+    }
 })
-
-
